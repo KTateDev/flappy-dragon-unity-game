9d4453e7a69aad630f3d2e1cf854d6898f199e19
Kyle <ktate2@ggc.edu>
2021-02-23 10:28:24 -0500

    Adding finished unity game

---
+++ Assets/ColumnController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColumnController : MonoBehaviour
+{
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+       GameObject g = Instantiate(gameObject);
+    
+       float xpos = transform.position.x;
+       Vector3 temp = new Vector3(xpos, Random.Range(0, 10f), 0);
+       g.transform.position = new Vector3(xpos + 40f, Random.Range(0, 10), 0);
+    
+    }
+}
---
+++ Assets/DragonController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class DragonController : MonoBehaviour
+{
+
+    public AudioClip crash;
+    public AudioClip flap;
+    public AudioClip point;
+    public AudioSource audioSource;
+    Rigidbody myBod;
+    public Animator anim;
+    public Text gameover;
+    public bool isPaused = false;
+    int score = 0;
+    Text scoreCounter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        scoreCounter = GameObject.Find("Score").GetComponent<Text>();
+
+        anim = GetComponent<Animator>();
+        myBod = GetComponent<Rigidbody>();
+        myBod.velocity = new Vector3(10,0,0);
+        audioSource = GetComponent<AudioSource>();
+        gameover = GameObject.Find("GameOver").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (Input.GetButtonDown("Jump")) {
+            if (isPaused == false)
+            {
+                jump();
+                anim.Play("Flap");
+                audioSource.PlayOneShot(flap, 0.7f);
+            }
+         
+            {
+                if (isPaused == true)
+                {
+                    Time.timeScale = 1;
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+                }
+            }
+        }
+
+    }
+
+    private void jump()
+    {     
+             myBod.velocity = new Vector3(10, 15, 0);
+       
+    }
+
+
+    void OnTriggerEnter(Collider other) {
+        
+        audioSource.PlayOneShot(point, 0.7f);
+        score++;
+        scoreCounter.text = "Score: " + score;
+
+    }
+
+    void OnCollisionEnter(Collision collision) {
+
+     
+        if (collision.gameObject.name == "Top" || collision.gameObject.name == "Bottom")
+        {
+            audioSource.PlayOneShot(crash, 0.7f);
+            Time.timeScale = 0;
+            gameover.text = "Game Over \n Press Jump to Play again";
+            isPaused = true;
+
+        }
+      
+    }
+}
